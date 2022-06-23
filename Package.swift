3c11663bc5389b73218e48b6a69c77fdbec9a057
dDomovoj <dmitryduleba@gmail.com>
2022-06-23 20:38:13 +0200

    [upd] package

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
             targets: ["NotificationCraftSystem"]),
     ],
     dependencies: [
-      .package(url: "https://github.com/dDomovoj/CallbacksCraft.git", branch: "main"),
+      .package(url: "https://github.com/dDomovoj/CallbacksCraft.git", branch: "master"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
